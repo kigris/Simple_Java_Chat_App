abba6a1cad8441923579567274e63fa5a6659236
Kigris <kigrislord@gmail.com>
2022-11-23 16:39:49 +0100

    added files

---
+++ clientChat.java
@@ -0,0 +1,92 @@
+import java.net.*;
+import java.io.*;
+
+// Hilo que escucha por los mensajes entrantes
+class ListenThread implements Runnable {
+	BufferedReader inputFromServer; // Referencia al buffer del servidor
+	Thread t;
+
+	public ListenThread(BufferedReader reader) {
+		t = new Thread(this);
+		inputFromServer = reader; // Se obtiene el buffer del servidor
+		t.start(); // Se inicia el hilo
+	}
+	
+	public void run() {
+		try { // Se intenta leer del servidor
+			while (true) {
+				// Se intenta leer del buffer del servidor
+				String message = inputFromServer.readLine();
+				if (message != null) { // Si hay mensaje que envió
+					System.out.println(message); // Se imprime
+				}
+			}
+		} catch (IOException e) { // Se capturan excepciones
+			System.out.println(e);
+		}
+	}
+}
+
+public class clientChat {
+	public static void main(String[] args) {
+		int port = 1500;
+		String server = "localhost";
+		Socket socket = null;
+		String msgToSend; // mensaje a ser enviado
+		BufferedReader input; // buffer lector
+		PrintWriter output; // buffer escritor
+		int ERROR = 1;
+
+		// Se leen argumentos
+		if (args.length == 2) { // Si hay dos argumentos
+			server = args[0]; // El primero es la dirección del servidor
+			try { // Se intenta obtener el puerto con el segundo argumento
+				port = Integer.parseInt(args[1]); // Se convierte a entero
+			} catch (Exception e) { // Se captura excepción
+				System.out.println("Error al intentar obtener el puerto");
+				System.out.println("Se ha establecido por defecto el puerto 1500");
+			}
+		}
+		try { // Se intenta conectar con el servidor
+			System.out.print("...Estableciendo conexión con el servidor...");
+			socket = new Socket(server, port);
+			System.out.println("Conectado con el servidor " +
+			socket.getInetAddress() +
+			":" + socket.getPort());
+		}
+		catch (UnknownHostException e) { // Se captura excepción de host desconocido
+			System.out.println(e);
+			System.exit(ERROR);
+		}
+		catch (IOException e) { // Se captura excepción de IO
+			System.out.println(e);
+			System.exit(ERROR);
+		}
+		try { // Se intentan abrir los buffers
+			// Se abre un buffer de lectura de la consola del cliente
+			InputStreamReader inputStream = new InputStreamReader(System.in);
+			input = new BufferedReader(inputStream);
+			// Se abre un buffer de escritura contra el servidor 
+			output = new PrintWriter(socket.getOutputStream(),true);
+			// Se abre un buffer de lectura del servidor, para los mensajes entrantes
+			// Se crea el hilo de escucha con un buffer de lectura del servidor, para mensajes entrantes
+			new ListenThread(new BufferedReader(new InputStreamReader(socket.getInputStream())));
+			// Se obtienen mensajes del cliente y se transmiten al servidor
+			while(true) {
+				while(input.ready()) {
+					msgToSend = input.readLine(); // Se obtiene el mensaje a enviar
+					output.println(msgToSend); // Se envía el mensaje al servidor
+				}
+			}
+		}
+		catch (IOException e) { // Se captura excepción de IO
+			System.out.println(e);
+		}
+		try { // Se intenta cerrar la conexión
+			socket.close();
+		}
+		catch (IOException e) { // Se captura excepción de IO
+			System.out.println(e);
+		}
+	}    
+}
---
+++ serverChat.java
@@ -0,0 +1,167 @@
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Stack;
+
+// Clase que se utiliza para agrupar mensaje, ip origen y puerto origen
+class Message {
+	// Campos privados
+	InetAddress address;
+	Integer port;
+	String message;
+
+	Message(InetAddress address_in, Integer port_in, String message_in) {
+		address = address_in;
+		port = port_in;
+		message = message_in;
+	}
+
+	// Métodos para obtener los campos privados
+	public InetAddress getAddress() {
+		return address;
+	}
+
+	public Integer getPort() {
+		return port;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+}
+
+// Hilo de red que se utiliza para establecer una conexión con el cliente
+class NetworkThread implements Runnable {
+	// Campos privados
+	Thread t;
+	Socket socket; // Campo para la referencia del socket establecido
+	Stack<Message> msgs; // Campo para la referencia del Stack de mensajes
+	BufferedReader input; // Campo para el buffer lector
+	PrintWriter output; // Campo para el buffer escritor
+	ArrayList<NetworkThread> threads;// Referencia a todos los hilos activos
+	ArrayList<Socket> connections; // Referencia a todas las conexiones activas
+
+	NetworkThread(Socket socket_in, Stack<Message> msgs_in, ArrayList<NetworkThread> threads_in,
+		ArrayList<Socket> connections_in) {
+		t = new Thread(this);
+		socket = socket_in;
+		msgs = msgs_in;
+		threads = threads_in;
+		connections = connections_in;
+
+		try { // Se intenta crear los bufferes
+				// Se inicializan los bufferes lectores y escritores
+			input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			output = new PrintWriter(socket.getOutputStream(), true);
+		} catch (IOException e) { // Se captura la excepcion
+			System.out.println(e);
+		}
+		t.start(); // Se inicia el hilo
+	}
+
+	public void join() {
+		try {t.join();}
+		catch (InterruptedException e) {
+			System.out.println(e);
+		}
+	}
+
+	// Método para enviar un mensaje al cliente
+	public void sendMessage(String msg) {
+		output.println(msg); // Se envía el mensaje a través del buffer de escritura
+	}
+
+	public void run() {
+		System.out.println("Nueva conexión de " + socket.getInetAddress() + ":" + socket.getPort());
+		try { // Se intenta leer del bufer de lectura
+			String msgToSend;
+			while (true) {
+				String message = input.readLine(); // Se espera una línea enviada del cliente
+				if (message == null)
+					break; // Si es nula, se sale del bucle
+				msgToSend = socket.getPort() + " " + message;
+				// Cuando se obtiene un mensaje se apila en el Stack de mensajes
+				msgs.push(new Message(socket.getInetAddress(), socket.getPort(), msgToSend));
+				System.out.println(msgToSend); // Se imprime el mensaje del cliente
+			}
+
+		} // Se captura la exepcion
+		catch (IOException e) {
+			System.out.println(e);
+		}
+		try { // Se intenta cerrar la conexión del cliente
+				// Llegados aquí significa que el cliente ha cerrado la conexión o ha ocurrido
+				// una excepción
+			socket.close(); // Se cierra la conexión
+			System.out.println("Conexión cerrada para el cliente " + socket.getInetAddress() + ":" + socket.getPort());
+		} // Se captura una posible excepción
+		catch (IOException e) {
+			System.out.println(e);
+		} finally { // Finalmente
+			connections.remove(socket); // Se elimina el socket de la lista de conexiones
+			threads.remove(this); // Se elimina el hilo de la lista de hilos
+		}
+	}
+}
+
+
+// Hilo que se usa para gestionar las notificaciones
+class NotifyThread implements Runnable {
+	Thread t;
+	ArrayList<NetworkThread> threads;// Referencia a todos los hilos activos
+	ArrayList<Socket> connections; // Referencia a todas las conexiones activas
+	Stack<Message> msgs; // Referencia al Stack de mensajes
+
+	NotifyThread(ArrayList<NetworkThread> threads_in, ArrayList<Socket> connections_in, Stack<Message> msgs_in) {
+		t = new Thread(this);
+		threads = threads_in; // Se obtiene la referencia a los hilos activos
+		connections = connections_in; // Se obtiene la referencia a las conexiones activas
+		msgs = msgs_in; // Se obtiene el Stack de mensajes
+		t.start(); // Se inicia el hilo
+	}
+
+	public void run() {
+		Message msg; // Mensaje a obtener
+		while (true) { // Se ejecuta siempre
+			if (!msgs.empty()) { // Si hay mensajes
+				msg = msgs.pop(); // Se obtiene el último mensaje
+				for (int i = 0; i < connections.size(); i++) { // Se itera sobre todas las conexiones
+					// Si no se trata del mismo cliente que lo envió
+					if (msg.getAddress() != connections.get(i).getInetAddress() &&
+							msg.getPort() != connections.get(i).getPort()) {
+						// Se envía el mensaje al cliente de la iteración actual
+						threads.get(i).sendMessage(msg.getMessage());
+					}
+				}
+			}
+		}
+	}
+}
+
+public class serverChat {
+	public static void main(String args[]) {
+		ArrayList<NetworkThread> threads = new ArrayList<>(); // Hilos de conexiones activas
+		ArrayList<Socket> connections = new ArrayList<>(); // Conexiones activas
+		ServerSocket server_socket; // El socket del servidor
+		int port = 1500; // Puerto
+		Stack<Message> msgs = new Stack<>(); // Pila de mensajes
+		try { // Se intenta aceptar una conexión de un cliente
+			server_socket = new ServerSocket(port); // Se inicia el socket del servidor
+			System.out.println("Servidor escuchando en el puerto " + server_socket.getLocalPort());
+			new NotifyThread(threads, connections, msgs); // Se crea el hilo de las notificaciones
+			while (true) { // Bucle infinito del servidor
+				Socket socket = server_socket.accept(); // Se obtiene la conexión con el cliente
+				connections.add(socket); // Se añade la conexión en la lista de conexiones
+				// Se crea y añade un hilo en la lista de hilos activos
+				threads.add(new NetworkThread(socket, msgs, threads, connections));
+			}
+		} catch (IOException e) { // Se captura una posible excepción
+			System.out.println(e);
+		} finally {
+			for (NetworkThread thread : threads) {
+				thread.join();
+			}
+		}
+	}
+}
